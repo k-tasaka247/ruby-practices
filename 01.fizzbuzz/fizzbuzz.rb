72377ada5d5da4ff98cdec89bcb82e11ec40515c
k-tasaka <k.tasaka@genbasupport.com>
2026-07-28 10:56:44 +0900

    あまりの計算を整数オブジェクトに修正、および余剰な条件を削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -5,11 +5,11 @@ count = 0
 20.times do
   count += 1
   case
-  when count % 3.0 == 0 && count % 5.0 == 0
+  when count % 3 == 0 && count % 5 == 0
     puts "FizzBuzz"
-  when count % 3.0 == 0 && count % 5.0 != 0
+  when count % 3 == 0
     puts "Fizz"
-  when count % 3.0 != 0 && count % 5.0 == 0
+  when count % 5 == 0
     puts "Buzz"
   else
     puts count
