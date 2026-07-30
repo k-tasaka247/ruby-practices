af6ef419acc738fb76cdfe578b105c07725f759d
k-tasaka <k.tasaka@genbasupport.com>
2026-07-30 16:26:07 +0900

    Fixed style from the advice of rubocop

--- 03.bowling/bow_score.rb
+++ 03.bowling/bow_score.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
-shots = Array.new
+shots = []
 
 scores.each do |shot|
   if shot == 'X'
@@ -17,16 +18,16 @@ frames = shots.each_slice(2).to_a
 point = 0
 
 (0..9).each do |c|
-  if frames[c][0] == 10
-    if frames[c+1][0] == 10
-      point += 20 + frames[c+2][0]
-    else
-      point += 10 + frames[c+1].sum
-    end
-  elsif frames[c].sum == 10
-    point += 10 + frames[c+1][0]
-  else
-    point += frames[c].sum
-  end
+  point += if frames[c][0] == 10
+             if frames[c + 1][0] == 10
+               20 + frames[c + 2][0]
+             else
+               10 + frames[c + 1].sum
+             end
+           elsif frames[c].sum == 10
+             10 + frames[c + 1][0]
+           else
+             frames[c].sum
+           end
 end
 puts point
