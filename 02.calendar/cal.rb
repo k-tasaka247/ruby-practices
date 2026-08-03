afcc7ab0803c3d802ffafe118e447946316b88f6
k-tasaka <k.tasaka@genbasupport.com>
2026-08-03 11:14:53 +0900

    レビューの修正指示への対応 - 空白挿入 - 変数名の修正 - 条件式の変更 - 三項演算子の利用 - 定数の設定

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,14 +3,14 @@ require "optparse"
 require "date"
 
 # Make Hash for Month and Year
-options={y: Date.today.year, m: Date.today.mon}
+options = { y: Date.today.year, m: Date.today.mon }
 
 # Option Variable
 opt = OptionParser.new
 
 # Set Options for `-m` and `-y`
-opt.on("-m MON", Integer){|v| options[:m] = v}
-opt.on("-y YEAR", Integer){|v| options[:y] = v}
+opt.on("-m MON", Integer){ |v| options[:m] = v }
+opt.on("-y YEAR", Integer){ |v| options[:y] = v }
 opt.parse!(ARGV)
 
 # Print Month, Year, Week Day
@@ -25,18 +25,15 @@ last_date = Date.new(options[:y], options[:m], -1)
 print "   " * first_date.wday
 
 # Print Days
-(first_date..last_date).each do |day| 
-  case day.wday
-  when 6
-    puts day.day.to_s.rjust(2)
+(first_date..last_date).each do |date| 
+  day = date.day.to_s.rjust(2)
+  if date.wday == 6
+    puts day
   else
-    print day.day.to_s.rjust(2)+" "
+    print day + " "
   end
 end
 
 # Line Break
-if first_date.wday + last_date.day < 35
-  print "\n\n"
-else
-  print "\n"
-end
+Standard_Week_Number = 5
+print first_date.wday + last_date.day < (7 * Standard_Week_Number) ? "\n\n" : "\n"
