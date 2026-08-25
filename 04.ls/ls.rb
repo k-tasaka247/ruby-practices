d898f91dbb2a5ac1377ac92b6cd54d7cc89d981b
k-tasaka <k.tasaka@genbasupport.com>
2026-08-25 14:28:47 +0900

    `-a`オプションの実装

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 require_relative './ls_methods'
 
-puts ls_print(ARGV[0])
+opt = OptionParser.new
+options = {}
+opt.on('-a') { |v| options[:a] = v }
+opt.parse!(ARGV)
+
+puts ls_print(options, ARGV[0])
