58cc8691bb51374403ce8a3bd9906e8bcf19437e
k-tasaka <k.tasaka@genbasupport.com>
2026-08-28 16:41:55 +0900

    特殊属性に対応、数値以外は左揃えになるように処理変更、定数名の再変更

--- 04.ls/ls_methods.rb
+++ 04.ls/ls_methods.rb
@@ -6,7 +6,35 @@ require 'date'
 PRINT_COLS_WIDTH = 3
 FILE_TYPE = { 'fifo' => 'p', 'characterSpecial' => 'c', 'directory' => 'd', 'blockSpecial' => 'b', 'file' => '-', 'link' => 'l', 'socket' => 's' }.freeze
 FILE_MODE = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
-LONGFORMAT_COL = 8
+S_FILE_MODE = { '0' => '--S', '1' => '--s', '2' => '-wS', '3' => '-ws', '4' => 'r-S', '5' => 'r-s', '6' => 'rwS', '7' => 'rws' }.freeze
+T_FILE_MODE = { '0' => '--T', '1' => '--t', '2' => '-wT', '3' => '-wt', '4' => 'r-T', '5' => 'r-t', '6' => 'rwT', '7' => 'rwt' }.freeze
+LONGFORMAT_COL_WITHOUT_FILE_NAME = 7
+
+def file_mode_judge(file)
+  mode = file.mode.to_s(8)
+  case mode[-4]
+  when '0'
+    (-3..-1).map { |index| FILE_MODE[mode[index]] }.join
+  when '1'
+    [-3, -2].map { |index| FILE_MODE[mode[index]] }.join + T_FILE_MODE[mode[-1]]
+  when '2'
+    FILE_MODE[mode[-3]] + S_FILE_MODE[mode[-2]] + FILE_MODE[mode[-1]]
+  when '3'
+    FILE_MODE[mode[-3]] + S_FILE_MODE[mode[-2]] + T_FILE_MODE[mode[-1]]
+  when '4'
+    S_FILE_MODE[mode[-3]] + [-2, -1].map { |index| FILE_MODE[mode[index]] }.join
+  when '5'
+    S_FILE_MODE[mode[-3]] + FILE_MODE[mode[-2]] + T_FILE_MODE[mode[-1]]
+  when '6'
+    [-3, -2].map { |index| S_FILE_MODE[mode[index]] }.join + FILE_MODE[mode[-1]]
+  when '7'
+    [-3, -2].map { |index| S_FILE_MODE[mode[index]] }.join + T_FILE_MODE[mode[-1]]
+  end
+end
+
+def number?(str)
+  str.match?(/^\s*[0-9]+$/)
+end
 
 def ls_elements_get(options, path)
   elements = options[:a] ? Dir.entries(path).sort : Dir.glob('*', base: path).sort
@@ -37,7 +65,7 @@ end
 def ls_details_get(path, element)
   elements_array = []
   file = File.lstat("#{path}/#{element}")
-  elements_array << FILE_TYPE[file.ftype] + (-3..-1).map { |index| FILE_MODE[file.mode.to_s(8)[index]] }.join
+  elements_array << FILE_TYPE[file.ftype] + file_mode_judge(file)
   elements_array << file.nlink.to_s
   elements_array << Etc.getpwuid(file.uid).name
   elements_array << Etc.getgrgid(file.gid).name
@@ -65,7 +93,11 @@ def ls_longformat_array_fix(options, path)
   end
 
   each_cols_max_length = elements_arrays_for_print.transpose.map { |col| col.map(&:size).max }
-  elements_arrays_for_print.each { |row| (LONGFORMAT_COL - 1).times { |col| row[col] = row[col].rjust(each_cols_max_length[col], ' ') } }
+  elements_arrays_for_print.each do |row|
+    LONGFORMAT_COL_WITHOUT_FILE_NAME.times do |col|
+      row[col] = number?(row[col]) ? row[col].rjust(each_cols_max_length[col], ' ') : row[col].ljust(each_cols_max_length[col], ' ')
+    end
+  end
 
   elements_arrays_for_print.unshift(["total #{total_block_size}"])
   [elements_arrays_for_print, each_cols_max_length]
