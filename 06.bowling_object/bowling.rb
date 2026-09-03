797cbaf97b73f94b27379ca8ab6e0da53a5a41ca
k-tasaka <k.tasaka@genbasupport.com>
2026-09-03 14:47:23 +0900

    オプションの説明を追加

--- 06.bowling_object/bowling.rb
+++ 06.bowling_object/bowling.rb
@@ -9,10 +9,10 @@ require_relative 'score'
 opt = OptionParser.new
 options = 'old'
 
-opt.on('-c', '--current') { options = 'current' }
+opt.on('-c', '--current') { options = 'current' } # 新ルールで計算するにはオプションを指定する
 
 opt.parse!(ARGV)
 game = Game.new(ARGV[0]) # ゲームの登録
 score = Score.new(game) # スコア計算準備
 
-puts score.result(options) # スコア出力（新ルールで計算するには引数に'current'を指定する）
+puts score.result(options) # スコア出力
